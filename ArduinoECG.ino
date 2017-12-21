53f177684f2f00a5c3a41f8598597ea2cb49c840
Bălănescu Adrian <balanescuadrian71@gmail.com>
2017-12-21 20:32:03 +0200

    added IO method, refactoring

--- ArduinoECG.ino
+++ ArduinoECG.ino
@@ -1,4 +1,3 @@
-
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 // Above for testing purposes
@@ -8,14 +7,18 @@
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 
-typedef enum {on, off, wait}sysState;
+typedef enum {on, off, wait}sysState; //
 
-class componentaSistem {
+class sysComponent {
 protected:
-	uint8_t pin;
+	uint8_t pin,IO;
 public:
-	componentaSistem(uint8_t pin) {
+	sysComponent(uint8_t pin) {
 		this->pin = pin;
+	}
+	
+	virtual void setIO(uint8_t IO) {
+		this->IO = IO;
 	}
 
 	virtual uint8_t getPin() {
@@ -26,12 +29,11 @@ public:
 	virtual void setOff() {};
 };
 
-class led:public componentaSistem {
+class led:public sysComponent {
 	sysState ledState;
 public:
-	led(uint8_t pin, sysState ledState) :componentaSistem(pin) {
+	led(uint8_t pin,sysState ledState) :sysComponent(pin) {
 		this->ledState = ledState;
-		pinMode(pin, OUTPUT);
 	}
 
 	void setOn() {
@@ -45,17 +47,17 @@ public:
 	}
 };
 
-class ecgSensor : public componentaSistem {
-	sysState state;
-	int value, loPlus, loMinus;
+class ecgSensor : public sysComponent {
+	sysState ecgState;
+	float value;
+	int loPlus, loMinus;
 	int loPlusPin, loMinusPin;
 public:
-	ecgSensor(uint8_t pin, int loPlusPin, int loMinusPin) :componentaSistem(pin) {
+	ecgSensor(uint8_t pin, int loPlusPin, int loMinusPin) :sysComponent(pin) {
 		this->pin = pin;
 		this->loPlusPin = loPlusPin;
 		this->loMinusPin = loMinusPin;
 
-		pinMode(pin, INPUT);
 		pinMode(loPlusPin, INPUT);
 		pinMode(loMinusPin, INPUT);
 	}
@@ -69,23 +71,23 @@ public:
 
 		if (loPlus == 1 || loMinus == 1) {
 			value = -1;
-			Serial.println(float(value));
-			state = wait;
+			Serial.println(value);
+			ecgState = wait;
 		}
 		else {
 			value = analogRead(pin);
 			Serial.println(float(value));
-			state = on;
+			ecgState = on;
 		}
 	}
 
 	sysState getState() {
-		return state;
+		return ecgState;
 	}
 };
 
-led ledRosu(2, off); //real 13
-led ledAlbastru(3, off); //real 12
+led ledRosu(2,off); //real 13
+led ledAlbastru(3,off); //real 12
 ecgSensor ecg(A1, 10, 11);
 
 void setup()
@@ -93,6 +95,9 @@ void setup()
 	Serial.begin(9600);
 	lcd.begin(16, 2);
 	lcd.clear();
+	ledRosu.setIO(OUTPUT);
+	ledAlbastru.setIO(OUTPUT);
+	ecg.setIO(INPUT);
 }
 
 void loop()
@@ -106,11 +111,13 @@ void loop()
 		ledRosu.setOn();
 		ledAlbastru.setOff();
 		lcd.setCursor(0, 0);
-		lcd.write("Waiting..");
+		lcd.write("Waiting for");
+		lcd.setCursor(0, 1);
+		lcd.write("signal...");
 		break;
 	case on: 
 		lcd.setCursor(0, 0);
-		lcd.write("Reading data");
+		lcd.write("Getting signal!");
 		ledRosu.setOff();
 		ledAlbastru.setOn();
 		ecg.readData();
